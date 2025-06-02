756ff366681c7de383379c5fd624c7cd8078bff1
Tipu-Sultan <teepukhan729@gmail.com>
2025-06-02 15:39:21 +0530

    check again

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,10 +1,23 @@
 import { NextResponse } from 'next/server';
-
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { CabikeUsers } from '@/models/cabike-schemas';
+import connectDB from '@/lib/db';
+import { sendVerificationEmail } from '@/utils/email';
+import { data } from 'autoprefixer';
 
 export async function POST(request) {
   try {
+    const { fullName, email, password } = await request.json();
+
+    // Validate input
+    if (!fullName || !email || !password) {
+      return NextResponse.json({ error: 'All fields are required' }, { status: 400 });
+    }
+
+
     return NextResponse.json(
-      { message: 'Registration successful. Please check your email to verify.' },
+      { message: 'Registration successful. Please check your email to verify.',data: { fullName, email } },
       { status: 201 }
     );
   } catch (error) {
